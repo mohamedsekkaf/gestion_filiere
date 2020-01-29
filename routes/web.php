4d5037b221911ace7511cf0aa2fb0567bfc4172e
mohamedsekkaf <mohamedsekkaf343@gmail.com>
2020-01-29 21:34:57 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,8 @@
 | contains the 'web' middleware group. Now create something great!
 |
 */
-Auth::routes();
-/*  Auth::routes(['register' => false]); */
+
+Auth::routes(['register' => false]);
 
 Route::get('/', function () {
     return view('home');
