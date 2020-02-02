6d32d560085e5f22033d3b85b9636064b8c5037e
mohamedsekkaf <mohamedsekkaf343@gmail.com>
2020-02-03 00:18:39 +0100

    09

--- app/Http/Controllers/etabliss.php
+++ app/Http/Controllers/etabliss.php
@@ -41,7 +41,7 @@ public function showinfo(){
 public function info($id)
 {
    $tes= DB::select("select * from etaplissemments where nom_etablessement = ?",[$id]);
-   $fil =  DB::select("select  from felieres where nom_etabless= ?",[$tes[0]->nom_etablessement])->paginate(2);
+   $fil =  DB::select("select * from felieres where nom_etabless= ?",[$tes[0]->nom_etablessement])->paginate(02);
    return  view('filiere', compact('fil'));
 }
 //========================================================== selection module
