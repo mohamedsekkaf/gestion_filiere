92ada17197e0b78d9bea58e0f94c62886404cdcb
mohamedsekkaf <mohamedsekkaf343@gmail.com>
2020-02-03 00:00:14 +0100

    09

--- storage/framework/views/2d17a7c66b051823657db2df8e862d2017b3a795.php
+++ storage/framework/views/2d17a7c66b051823657db2df8e862d2017b3a795.php
@@ -79,7 +79,6 @@
           <a class="button button--margin-right button--hover" href="<?php echo e(url('/login')); ?>">Login</a>
           <a class="button button--hover" href="<?php echo e(url('/register')); ?>" >Register</a>
           <?php else: ?>
-
           <div class="homenav">
             <a class="button button--hover" href="<?php echo e(url('/logout')); ?>" >logout</a>
                 </div>
@@ -115,16 +114,16 @@
 </div>
     </section>
     
- <!--    <section class="contact contact--bg">
+    <section class="contact contact--bg">
       <div class="container">
         <div class="page-section text-center">
-          <h2 class="page-section__title--white"></h2>
-          <p class="page-section__paragraph--white">test</p>
-          <a class="button button--hover" href="#">LETS GO</a>
+          <h2 class="page-section__title--white">Les établissement de Université chouaib doukkali</h2>
+          <p class="page-section__paragraph--white">Gérer le site</p>
+          <a class="button button--hover" href="http://gestion-fil-estsb.herokuapp.com/ajouter">LETS GO</a>
         </div>
       </div>
     </section>
-    
+   <!--  
     <section class="about">
       <div class="container">
         <div class="page-section">
@@ -173,82 +172,7 @@
      
      -->
    
-      
-   
-      
-      <footer class="footer footer--bg">
-        <div class="container">
-          <div class="page-section">
-            <div class="row gutters-100">
-              <div class="col-md-4 col-lg-3">
-                <div class="footer__first">
-                  <h2 class="footer__title"></h2>
-                  <p class="footer-first__paragraph"> </p>
-                  <ul class="footer-first__social-icons">
-                    <li><a href="#"><i class="flaticon-facebook-letter-logo"></i></a></li>
-                    <li><a href="#"><i class="flaticon-twitter-logo"></i></a></li>
-                    <li><a href="#"><i class="flaticon-dribbble-logo"></i></a></li>
-                    <li><a href="#"><i class="flaticon-behance-logo"></i></a></li>
-                  </ul>
-                </div>
-              </div>
-              <div class="col-md-6 col-lg-2">
-                <div class="footer__second">
-                  <h2 class="footer__title">links</h2>
-                  <ul>
-                    <li><a href="">Home</a></li>
-                  </ul>
-                </div>
-              </div>
-              <div class="col-md-6 col-lg-3">
-                <div class="footer__third">
-                  <h2 class="footer__title">CONTACT US</h2>
-                  <ul>
-                    <li><span class="glyphicon glyphicon-envelope"></span> <a href="#">mohamedsekkaf343@gmail.com</a></li>
-                    <li><span class="glyphicon glyphicon-earphone"></span> <a href="#">+"2126-36-67-92-09</a></li>
-                  </ul>
-                  <h4 class="footer__subscribe__title">Subscribe</h4>
-                  <div class="subscribe-section">
-                    <input type="email" class="form-control" size="50" placeholder="Enter Your Email" required>
-                    <button class="subscribe-section__button" type="button"><img src="assets/images/send-icon.png" alt=""></button>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-6 col-lg-4">
-                <div class="footer__fourth">
-                  <h4 class="footer__title">INSTAGRAM</h4>
-                  <div class="row">
-                    <ul>
-                      <li><a href="#"><img src="assets/images/instagram-pic1.png" alt=""></a></li>
-                      <li><a href="#"><img src="assets/images/instagram-pic2.png" alt=""></a></li>
-                      <li><a href="#"><img src="assets/images/instagram-pic3.png" alt=""></a></li>
-                    </ul>
-                  </div>
-                  <div class="row">
-                    <ul>
-                      <li><a href="#"><img src="assets/images/instagram-pic4.png" alt=""></a></li>
-                      <li><a href="#"><img src="assets/images/instagram-pic5.png" alt=""></a></li>
-                      <li><a href="#"><img src="assets/images/instagram-pic6.png" alt=""></a></li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <hr class="footer__horizontal-bar">
-            <p class="footer__bottom-paragraph">&copy; Copyright 2020 <a href="#" style="color: #00aafe;font-weight:bold;">MS</a>. All Rights Reserved</p>
-          </div>
-        </div>
-      </footer>
   </div>
-    
-    
-
-
-
-
-
-
-
 
 
 <?php $__env->stopSection(); ?>
