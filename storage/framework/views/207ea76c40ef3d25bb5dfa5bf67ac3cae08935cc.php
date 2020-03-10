c39ecefd13b68381ff2ad486942ff51d70cf723b
mohamedsekkaf <mohamedsekkaf343@gmail.com>
2020-03-10 14:10:41 +0100

    55

--- storage/framework/views/207ea76c40ef3d25bb5dfa5bf67ac3cae08935cc.php
+++ storage/framework/views/207ea76c40ef3d25bb5dfa5bf67ac3cae08935cc.php
@@ -14,45 +14,13 @@
 </head>
 
 <body>
-<table>
-<tr>
-<td>test</td>
-<td>test</td>
-<td>test</td>
-<td>test</td>
-<td>test</td>ù<td>test</td>
-</tr>
-<tr>
-<td>test</td>
-<td>test</td>
-<td>test</td>
-<td>test</td>
-<td>test</td>ù<td>test</td>
-</tr>
-<tr>
-<td>test</td>
-<td>test</td>
-<td>test</td>
-<td>test</td>
-<td>test</td>ù<td>test</td>
-</tr>
-<tr>
-<td>test</td>
-<td>test</td>
-<td>test</td>
-<td>test</td>
-<td>test</td>ù<td>test</td>
-</tr>
-</table>
     <h1 id="title">  <?php  foreach($sem as $s){ echo $s->nom_etabless ;break; }?> Filiere <?php  foreach($sem as $s){ echo $s->nom_file ;break; }?></h1>
         <?php 
- foreach($sem as $s){
-    
-    echo '<table><tr><td style="border;1px solid red;"><h1> Semestre : '.substr($s->nom_s,0,1) .'</h1></td>';
-    echo '</tr>';
+ foreach($sem as $s){ 
+    echo '<h2> Semestre : '.substr($s->nom_s,0,1) .'</h2>';
     foreach($mod as $m ){
         if($s->nom_s==$m->nom_se){
-            echo '<h3 id="tt" style="margin-left:50px;">Module : '.$m->nom_module.'</h3>';
+            echo '<h3 id="tt"  style="margin-left:50px" >Module : '.$m->nom_module.'</h3>';
         }
         foreach($elem as $e){
             if($m->nom_module==$e->nom_mod && $s->nom_s==$e->nom_sem){
