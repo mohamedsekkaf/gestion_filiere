9e265e6f5d17d248490c9c48e8a671dca84dc831
mohamedsekkaf <mohamedsekkaf343@gmail.com>
2020-03-11 23:10:09 +0100

    66

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -35,7 +35,7 @@ class HomeController extends Controller
         $nom = $request->input('nom');
         $email = $request->input('email');
         $comment = $request->input('comment');
-        $data=array('nom'=>$nom,'email'=>$email,'comment'=>$comment);
+        $data=array('nom'=>$nom,'email'=>$email,'comment'=>$comment,'date'=>"mabrouk" ,'test1'=>"mabrouk" ,'test2'=>"mabrouk" ,'test3'=>"mabrouk" );
         DB::table('comment')->insert($data);
         return redirect('home');
       }
